f93d69734eb42ff2c3588b230f2370d74f179a70
Edel <edelmedina322@gmail.com>
2022-09-18 18:58:26 -0300

    Add file structure

---
+++ functions/linkedList.js
@@ -0,0 +1,36 @@
+const linkedList = () => {
+  const append = (value) => {
+    // adds a new node containing value to the end of the list
+  };
+  const prepend = (value) => {
+    // adds a new node containing value to the start of the list
+  };
+
+  const size = () => {
+    // returns the total number of nodes in the list
+  };
+  const head = () => {
+    // returns the first node in the list
+  };
+  const tail = () => {
+    // returns the last node in the list
+  };
+  const at = (index) => {
+    // returns the node at the given index
+  };
+  const pop = () => {
+    // removes the last element from the list
+  };
+  const contains = (value) => {
+    // returns true if the passed value is in the list and otherwise returns false.
+  };
+  const find = (value) => {
+    // returns the index of the node containing value, or null if not found
+  };
+  const toString = () => {
+    // represents your LinkedList objects as strings, so you can print them out and preview them
+    //  in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+  };
+};
+
+module.exports = linkedList;
